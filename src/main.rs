2eea1ae5299c567c310bdbba6b5f9a29264470ac
koyashiro <develop@koyashi.ro>
2022-01-12 01:10:29 +0900

    feat: implement clipboard operations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use anyhow::{ensure, Result};
+use anyhow::{anyhow, ensure, Result};
+
+use windows::Win32::Foundation::{HANDLE, PSTR};
+use windows::Win32::Globalization::lstrcpyA;
+use windows::Win32::System::DataExchange::{
+    CloseClipboard, EmptyClipboard, OpenClipboard, SetClipboardData,
+};
+use windows::Win32::System::Memory::{GlobalAlloc, GlobalLock, GlobalUnlock};
 use windows::Win32::UI::Input::KeyboardAndMouse::{
     MapVirtualKeyA, SendInput, INPUT, INPUT_0, KEYBDINPUT,
 };
@@ -6,13 +13,38 @@ use windows::Win32::UI::WindowsAndMessaging::{FindWindowA, SetForegroundWindow};
 
 fn find_window_by_name(name: &str) -> Result<isize> {
     let hwnd = unsafe { FindWindowA(None, name) };
-    ensure!(hwnd != 0, "VRChat.exe does not exists");
+    ensure!(hwnd != 0, "`{}` does not exists", name);
     Ok(hwnd)
 }
 
 fn set_foreground_window(hwnd: isize) -> Result<()> {
     let result = unsafe { SetForegroundWindow(hwnd) };
     ensure!(result.as_bool(), "failed to set foreground window");
+    Ok(())
+}
+
+fn set_clipboard(value: &str) -> Result<()> {
+    let result = unsafe { OpenClipboard(None) };
+    ensure!(result.as_bool(), "failed to open clipboard");
+
+    let result = unsafe { EmptyClipboard() };
+    ensure!(result.as_bool(), "failed to initialize clipboard");
+
+    const GHND: u32 = 0x0042;
+    let hmem = unsafe { GlobalAlloc(GHND, value.len() + 1) };
+
+    let lpstring1 = PSTR(unsafe { GlobalLock(hmem) as _ });
+    unsafe { lstrcpyA(lpstring1, value) };
+    let result = unsafe { GlobalUnlock(hmem) };
+    ensure!(!result.as_bool(), "failed to unlock memory");
+
+    const CF_TEXT: u32 = 1;
+    let handle = unsafe { SetClipboardData(CF_TEXT, HANDLE(hmem)) };
+    ensure!(!handle.is_invalid(), "failed to set data to clipboard");
+
+    let result = unsafe { CloseClipboard() };
+    ensure!(result.as_bool(), "failed to close clipboard");
+
     Ok(())
 }
 
@@ -117,9 +149,14 @@ fn send_enter_input() -> Result<()> {
 }
 
 fn main() -> Result<()> {
+    let url = std::env::args()
+        .nth(1)
+        .ok_or(anyhow!("argument is required"))?;
+
     let hwnd = find_window_by_name("VRChat")?;
     set_foreground_window(hwnd)?;
 
+    set_clipboard(&url)?;
     send_paste_input()?;
     send_enter_input()?;
 
