853b6c0bf6131ab9917df21e612c65cd56f804a9
koyashiro <develop@koyashi.ro>
2022-01-11 23:00:22 +0900

    refactor: refactor main function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
+use anyhow::{ensure, Result};
 use windows::Win32::UI::Input::KeyboardAndMouse::{
     MapVirtualKeyA, SendInput, INPUT, INPUT_0, KEYBDINPUT,
 };
 use windows::Win32::UI::WindowsAndMessaging::{FindWindowA, SetForegroundWindow};
 
-fn main() {
-    let hwnd = unsafe { FindWindowA(None, "VRChat") };
-    let result = unsafe { SetForegroundWindow(hwnd) };
-    dbg!(result.as_bool());
+fn find_window_by_name(name: &str) -> Result<isize> {
+    let hwnd = unsafe { FindWindowA(None, name) };
+    ensure!(hwnd != 0, "VRChat.exe does not exists");
+    Ok(hwnd)
+}
 
+fn set_foreground_window(hwnd: isize) -> Result<()> {
+    let result = unsafe { SetForegroundWindow(hwnd) };
+    ensure!(result.as_bool(), "failed to set foreground window");
+    Ok(())
+}
+
+fn send_input(inputs: &[INPUT]) -> Result<()> {
+    let result = unsafe {
+        SendInput(
+            inputs.len() as u32,
+            inputs.as_ptr(),
+            std::mem::size_of::<INPUT>() as i32,
+        )
+    };
+    ensure!(result != 0);
+    Ok(())
+}
+
+fn send_paste_input() -> Result<()> {
     let inputs = [
         INPUT {
             r#type: 0x0001, // INPUT_KEYBOARD
@@ -57,6 +78,14 @@ fn main() {
                 },
             },
         },
+    ];
+    let result = send_input(&inputs);
+    ensure!(result.is_ok(), "failed to send `Paste`(`Ctrl` + `V`) input");
+    Ok(())
+}
+
+fn send_enter_input() -> Result<()> {
+    let inputs = [
         INPUT {
             r#type: 0x0001, // INPUT_KEYBOARD
             Anonymous: INPUT_0 {
@@ -82,13 +111,17 @@ fn main() {
             },
         },
     ];
+    let result = send_input(&inputs);
+    ensure!(result.is_ok(), "failed to send `Enter` input");
+    Ok(())
+}
 
-    let result = unsafe {
-        SendInput(
-            inputs.len() as u32,
-            inputs.as_ptr(),
-            std::mem::size_of::<INPUT>() as i32,
-        )
-    };
-    dbg!(result);
+fn main() -> Result<()> {
+    let hwnd = find_window_by_name("VRChat")?;
+    set_foreground_window(hwnd)?;
+
+    send_paste_input()?;
+    send_enter_input()?;
+
+    Ok(())
 }
