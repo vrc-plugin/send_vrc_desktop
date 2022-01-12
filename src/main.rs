fb9553ed98cdd2066efe51afe3c6358d1e427076
koyashiro <develop@koyashi.ro>
2022-01-13 00:44:43 +0900

    refactor: change default port to 11400

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use handle::url;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(url))
-        .bind("127.0.0.1:8080")?
+        .bind("127.0.0.1:11400")?
         .run()
         .await
 }
