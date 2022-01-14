2d15c804d2f2811946fcb8526bcc414a90383bbf
koyashiro <develop@koyashi.ro>
2022-01-14 12:43:57 +0900

    refactor: change HTTP method from post to put

--- src/router.rs
+++ src/router.rs
@@ -1,7 +1,7 @@
-use axum::{routing::post, Router};
+use axum::{routing::put, Router};
 
 use crate::handle;
 
 pub fn new() -> Router {
-    Router::new().route("/url", post(handle::url))
+    Router::new().route("/url", put(handle::url))
 }
