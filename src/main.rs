20d3efaabae48b1edf3eebd53d37f9c5756cf748
koyashiro <develop@koyashi.ro>
2022-01-15 17:55:23 +0900

    feat: use `PORT` environment variable

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,26 @@ mod handle;
 mod router;
 mod win32api;
 
+use std::env;
 use std::net::SocketAddr;
 
+use anyhow::Result;
+
+const DEFAULT_PORT: u16 = 11400;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let app = router::new();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 11400));
+
+    let port = env::var("PORT")
+        .map(|p| p.parse().unwrap_or(DEFAULT_PORT))
+        .unwrap_or(DEFAULT_PORT);
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
